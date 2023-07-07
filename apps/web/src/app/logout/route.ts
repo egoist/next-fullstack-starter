0e0c37d6a66355c5828b2df66d287a2748bdacfd
EGOIST <hi@egoist.dev>
2023-07-08 01:23:26 +0800

    tweaks

--- apps/web/src/app/logout/route.ts
+++ apps/web/src/app/logout/route.ts
@@ -2,7 +2,6 @@ import { NextRequest, NextResponse } from "next/server"
 import { env } from "~/server/env"
 
 export const GET = (request: NextRequest) => {
-  request.cookies.delete(env.AUTH_COOKIE_NAME)
   const url = request.nextUrl.clone()
   url.pathname = "/"
   const res = NextResponse.redirect(url)
